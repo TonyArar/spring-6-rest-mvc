cfd69f6f7653996e30271e490dd2492d0c393a64
TonyArar <tonyirar@gmail.com>
2025-06-04 18:04:45 +0200

    fixed test and learned more about how hibernate processes operations

--- src/test/java/com/spring/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/spring/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.spring.spring_6_rest_mvc.repositories;
 
 import com.spring.spring_6_rest_mvc.entities.Beer;
+import com.spring.spring_6_rest_mvc.models.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,8 +27,20 @@ class BeerRepositoryTest {
         Beer beerToBeSaved = Beer
                 .builder()
                 .beerName("My Beer")
+                .upc("012345678910")
+                .beerStyle(BeerStyle.PILSNER)
+                .price(BigDecimal.valueOf(777))
+                .quantityOnHand(3)
                 .build();
         Beer savedBeer = beerRepository.save(beerToBeSaved);
+        // from flush() docs: flushes all pending changes to the database
+        // call flush so that this won't happen:
+        // the test will pass on invalid beer fields if we don't call flush()
+        // after saving the beer, because saving is batch-processed and test
+        // finishes execution before saving is processed, so validation exception
+        // flies under the test's radar unnoticed and the test passes
+        // (evil and silent semantically incorrect behaviour)
+        beerRepository.flush();
         assertNotNull(savedBeer);
         assertEquals("My Beer", savedBeer.getBeerName());
         assertNotNull(savedBeer.getId());
